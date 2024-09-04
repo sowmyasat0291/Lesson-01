eafa12acfbe37ebe6e80c973bea3bdd7b2c4380a
Sowmya <sowmyam0291@gmail.com>
2024-09-04 15:21:08 +0530

    Test Commit

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+def posts_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/posts_page.html', {'posts': post})
