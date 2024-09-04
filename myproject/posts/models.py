d31428c2518315ff555d4573051388ec9f2e2932
Sowmya <sowmyam0291@gmail.com>
2024-09-04 14:22:23 +0530

    Test Commit

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
@@ -6,3 +7,6 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
