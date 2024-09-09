0ddcbdb453de2afa3d38d4a8491f87c24674db50
Sowmya <sowmyam0291@gmail.com>
2024-09-09 15:07:14 +0530

    Test Commit

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
-
+from . import forms
 
 # Create your views here.
 def posts_list(request):
@@ -12,4 +12,13 @@ def posts_page(request, slug):
     return render(request, 'posts/posts_page.html', {'post': post})
 @login_required(login_url="/users/login")
 def post_new(request):
-    return render(request, 'posts/post_new.html') 
+    if request.method == 'POST': 
+        form = forms.CreatePost(request.POST, request.FILES) 
+        if form.is_valid():
+            newpost = form.save(commit=False) 
+            newpost.author = request.user 
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', { 'form': form })
